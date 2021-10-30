802c045d9289b922d7e620075a48a114519029ea
Ola Bamisaiye <code@olab.dev>
2021-10-30 11:47:28 -0400

    Workshop 1; Task #3 Completed...
    by updating "week1\workshop1\workshop.sql"

--- week1/workshop1/workshop.sql
+++ week1/workshop1/workshop.sql
@@ -18,7 +18,6 @@ SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
 
-
 ---
 --- CREATE tables
 ---
@@ -51,6 +50,13 @@ CREATE TABLE supplier (
 CREATE TABLE customers (
     id SERIAL,
     company_name TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employees (
+    id SERIAL,
+    first_name TEXT,
+    last_name TEXT,
     PRIMARY KEY(id)
 );
 
